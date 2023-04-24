06638e0f5088313f25a68904918ec88834d1cf41
Pavlo Antoniv <pavlivavli@gmail.com>
2023-04-24 15:52:09 +0200

    Update PixabayAPI.js

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -4,7 +4,7 @@ import { KEY } from './api-key.js';
 
 export class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/api/';
-  // #API_KEY = '34991535-a7425182e30d9d17c0e128526';
+  // #API_KEY = '35725736-1d55e9c3004cf0c0340b411f8';
   #query = '';
 
   constructor() {
@@ -37,27 +37,3 @@ export class PixabayAPI {
     this.#query = newQuery;
   }
 }
-
-// ------- 2 variant ---------
-
-//*  Перевірка помилок під час виконання запиту до серверу
-
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.interceptors.response.use(
-//   response => {
-//     return response;
-//   },
-//   error => {
-//     Notiflix.Notify.failure('Something went wrong. Please try again later.');
-//     return Promise.reject(error);
-//   },
-// );
-
-// async function fetchImages(query, page, perPage) {
-//   const response = await axios.get(
-//     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-//   );
-//   return response.data;
-// }
-
-// export { fetchImages };
